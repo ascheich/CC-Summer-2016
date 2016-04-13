2990c7b267e1c2eea860b41f4bef72f6600d4782
Andreas Scheicher <s1000239@stud.sbg.ac.at>
2016-04-13 16:46:22 +0200

    cropped weird rightshift if-tree

--- selfie.c
+++ selfie.c
@@ -1209,21 +1209,18 @@ int leftShift(int n, int b) {
 int rightShift(int n, int b) {
     // assert: b >= 0
 
-     if (n >= 0) {
-        if (b < 31)
-            return n >> b;
-        else
-            return 0;
-    } else if (b < 31)
-        // works even if n == INT_MIN:
-        // shift right n with msb reset and then restore msb
-        return ((((n + 1) + INT_MAX) >> b ) +
-            ((INT_MAX >> b) + 1));
-    else if (b == 31)
-        return 1;
-    else
+    if (b < 0)
         return 0;
+    
+    if (b > 30)
+        return 0;
+    
+    if (n >= 0)
+        return (n >> b);
+    else
+        return ((((n + 1) + INT_MAX) >> b) + ((INT_MAX >> b) + 1));
 }
+
 
 int loadCharacter(int *s, int i) {
     // assert: i >= 0
